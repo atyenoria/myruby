750901681da111dd70d3cb70d46db7e3dc5a0493
atyenoria <eigotyoubunn30@gmail.com>
2015-06-03 03:59:32 +0900

    first

---
+++ get_gresult.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/ruby
+# -*- encoding: utf-8 -*-
+require "rubygems"
+require "nokogiri"
+require "open-uri"
+require "cgi"
+n=0
+keyword = ARGV[0]
+num_tmp=ARGV[1]
+num=num_tmp.to_i - 1
+escaped_keyword = CGI.escape(keyword)
+url="http://www.google.co.jp/search?ie=UTF-8&oe=UTF-8&q=#{escaped_keyword}"
+html = open(url, "r:CP51932").read
+
+doc = Nokogiri::HTML.parse(html, 'CP51932')
+doc.search("div#search ol li").each_with_index do |li, idx|
+href = ""
+html = ""
+link = ""
+# aタグの中身を解析
+li.search("h3 a").each do |alink|
+href = alink.attribute("href")
+URI.parse(href).query.split(/&/).each do |str|
+strs = str.split(/=/)
+
+link = strs[1] if strs[0] == "q"
+
+end
+html = alink.inner_html
+end
+
+# Webサイト以外（ニュースなど）はスキップ
+next if "#{href}" !~ /^\/url/
+
+# puts "idx: #{idx+1}"
+# puts "domain: #{link.split("/")[2]}"
+# puts "link: #{link}"
+# puts "html: #{html}"
+# puts ""
+link=URI.unescape("#{link}")
+
+system( "chrome_tmp #{link} 1>&2 > /dev/null")
+n=n+1
+if n > num then
+	break
+end
+
+
+end
+
+
+
+
